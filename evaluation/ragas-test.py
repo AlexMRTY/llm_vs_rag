54a2f05cdf3bcb4289857848ea1a2eb9ed16a6dd
AlexMRTY <asifrezai261@gmail.com>
2025-04-26 13:08:29 +0200

    ragas-test automation

--- evaluation/ragas-test.py
+++ evaluation/ragas-test.py
@@ -20,21 +20,32 @@ def split_documents(contexts):
     
     return documents
 
-QA_PAIR_PATH = "data/llama3.1:8b-instruct-fp16_k1.jsonl"
-samples = {}
-with open(QA_PAIR_PATH, "r", encoding="utf-8") as f:
-    count = 0
-    for line in f:
-        if count > 3: break;
-        data = json.loads(line.strip())
-        if (data.get("response")):
-            continue
-        samples[data['id']] = SingleTurnSample(
-            user_input=data["question"],
-            response=data["answer"],
-            reference=data["expected_answer"]
-        )
-        count += 1
+# QA_PAIR_PATH = "results/llama3.1:8b-instruct-fp16_k1.jsonl"
+qa_results = [
+    "results/llama3.1:8b-instruct-fp16_k1.jsonl",
+    "results/llama3.1:8b-instruct-fp16_k2.jsonl",
+    "results/llama3.1:8b-instruct-fp16_k3.jsonl",
+    "results/llama3.1:8b-instruct-fp16_k4.jsonl",
+    "results/llama3.1:8b-instruct-fp16_k5.jsonl",
+]
+samples_collection = {}
+for path in qa_results:
+    with open(path, "r", encoding="utf-8") as f:
+        samples = {}
+        # count = 0
+        for line in f:
+            # if count > 3: break;
+            data = json.loads(line.strip())
+            if (data.get("response")):
+                continue
+            samples[data['id']] = SingleTurnSample(
+                user_input=data["question"],
+                response=data["answer"],
+                reference=data["expected_answer"]
+            )
+            # count += 1
+        samples_collection[path] = samples
+    
 
 
 
@@ -59,15 +70,38 @@ from tqdm import tqdm
 llm = LangchainLLMWrapper(OllamaLLM(model="qwen2.5:14b-instruct"))
 embeddings = LangchainEmbeddingsWrapper(OllamaEmbeddings(model="nomic-embed-text:latest"))
 
-# dataset = EvaluationDataset(samples)
+"""
+    Measures answer accuracy compared to ground truth given a user_input.
+    This metric averages two distinct judge prompts to evaluate.
 
+    Top10, Zero-shoot LLM-as-a-Judge Leaderboard:
+    1)- mistralai/mixtral-8x22b-instruct-v0.1
+    2)- mistralai/mixtral-8x7b-instruct-v0.1
+    3)- meta/llama-3.1-70b-instruct
+    4)- meta/llama-3.3-70b-instruct
+    5)- meta/llama-3.1-405b-instruct
+    6)- mistralai/mistral-nemo-12b-instruct
+    7)- nvidia/llama-3.1-nemotron-70b-instruct
+    8)- meta/llama-3.1-8b-instruct
+    9)- google/gemma-2-2b-it
+    10)- nvidia/nemotron-mini-4b-instruct
+    The top1 LB model have high correlation with human judges (~0.90).
+
+    Attributes
+    ----------
+    name: string
+        The name of the metrics
+
+    answer_accuracy:
+        The AnswerAccuracy object
+    """
 import asyncio
-async def eval_answer_accuracy():
+async def eval_answer_accuracy(dataset):
     scorer = AnswerAccuracy(llm=llm) # evaluator_llm wrapped with ragas LLM Wrapper
     scores = []
-    with tqdm(total=len(samples.keys()), unit="sample") as pbar:
-        for sampleKey in samples.keys():
-            sample = samples[sampleKey]
+    with tqdm(total=len(dataset.keys()), unit="sample") as pbar:
+        for sampleKey in dataset.keys():
+            sample = dataset[sampleKey]
             score = await scorer.single_turn_ascore(sample)
             scores.append({
                 "id": sampleKey,
@@ -81,10 +115,15 @@ async def eval_answer_accuracy():
     
     return scores
 
-result_answer_accuracy = asyncio.run(eval_answer_accuracy())
-# print("Score: ", score)
-# result = evaluate(dataset, metrics=[AnswerAccuracy], llm=llm,embeddings=embeddings)
+result_collection = {}
+for sample_name in samples_collection.keys():
+    samples = samples_collection[sample_name]
+    result_collection[sample_name] = asyncio.run(eval_answer_accuracy(samples))
+
 
 # Save Results to csv
-df = pd.DataFrame(result_answer_accuracy)
-df.to_csv("data/ragas-result-test.csv", index=False)
+for sample_name in result_collection.keys():
+    df = pd.DataFrame(result_collection[sample_name])
+    df.to_csv(f"results/evaluations/{sample_name}_answer_accuracy.csv", index=False)
+    print(f"Results for {sample_name} saved to CSV.")
+
